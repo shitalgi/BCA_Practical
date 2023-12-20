1aef4e6f412892e691d470374b9a53353b301b08
shitalgi <shresthashital706@gmail.com>
2023-12-20 14:45:59 +0545

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace BCA_Practical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPersonName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCountryName = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnIsEqual = new System.Windows.Forms.Button();
+            this.btnAssignment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Person Name";
+            // 
+            // txtPersonName
+            // 
+            this.txtPersonName.Location = new System.Drawing.Point(16, 40);
+            this.txtPersonName.Name = "txtPersonName";
+            this.txtPersonName.Size = new System.Drawing.Size(129, 20);
+            this.txtPersonName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Country Name";
+            // 
+            // txtCountryName
+            // 
+            this.txtCountryName.Location = new System.Drawing.Point(151, 40);
+            this.txtCountryName.Name = "txtCountryName";
+            this.txtCountryName.Size = new System.Drawing.Size(123, 20);
+            this.txtCountryName.TabIndex = 3;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(280, 32);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(47, 34);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSum.Location = new System.Drawing.Point(280, 86);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(60, 34);
+            this.btnSum.TabIndex = 9;
+            this.btnSum.Text = "SUM";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(151, 94);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(123, 20);
+            this.txtSecondNumber.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Second Number";
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(16, 94);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(129, 20);
+            this.txtFirstNumber.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "First Number";
+            // 
+            // btnIsEqual
+            // 
+            this.btnIsEqual.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIsEqual.Location = new System.Drawing.Point(12, 120);
+            this.btnIsEqual.Name = "btnIsEqual";
+            this.btnIsEqual.Size = new System.Drawing.Size(91, 32);
+            this.btnIsEqual.TabIndex = 10;
+            this.btnIsEqual.Text = "IS EQUAL";
+            this.btnIsEqual.UseVisualStyleBackColor = true;
+            this.btnIsEqual.Click += new System.EventHandler(this.btnIsEqual_Click);
+            // 
+            // btnAssignment
+            // 
+            this.btnAssignment.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAssignment.Location = new System.Drawing.Point(109, 120);
+            this.btnAssignment.Name = "btnAssignment";
+            this.btnAssignment.Size = new System.Drawing.Size(91, 32);
+            this.btnAssignment.TabIndex = 11;
+            this.btnAssignment.Text = "Assignment";
+            this.btnAssignment.UseVisualStyleBackColor = true;
+            this.btnAssignment.Click += new System.EventHandler(this.btnAssignment_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 193);
+            this.Controls.Add(this.btnAssignment);
+            this.Controls.Add(this.btnIsEqual);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtCountryName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPersonName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPersonName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCountryName;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnIsEqual;
+        private System.Windows.Forms.Button btnAssignment;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BCA_Practical
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string pn = txtPersonName.Text;
+            string cn = txtCountryName.Text;
+
+            if (string.IsNullOrEmpty(pn))  // pn == ""
+            {
+                MessageBox.Show("Enter Person Name");
+            }
+            else if (string.IsNullOrEmpty(cn))
+            {
+                MessageBox.Show("Enter Country Name");
+            }
+            else
+            {
+                //string msg = "Hello " + pn + ". Welcome to " + cn;
+                string msg = string.Format("Hello {0}. Welcome to {1}. Have fun {0}!!"
+                                        , pn, cn);
+                MessageBox.Show(msg);
+            }
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int fn = Convert.ToInt32(txtFirstNumber.Text);
+            int sn = Convert.ToInt32(txtSecondNumber.Text);
+
+            int sum = fn + sn;
+            string msg = "The sum of two number is " + sum.ToString();
+            MessageBox.Show(msg, "SUM");
+        }
+
+        private void btnIsEqual_Click(object sender, EventArgs e)
+        {
+            int fn = Convert.ToInt32(txtFirstNumber.Text);
+            int sn = Convert.ToInt32(txtSecondNumber.Text);
+
+            string msg = fn == sn ? "The numbers are EQUAL" : "NOT EQUAL";
+            MessageBox.Show(msg);
+        }
+
+        private void btnAssignment_Click(object sender, EventArgs e)
+        {
+            int fn = Convert.ToInt32(txtFirstNumber.Text);
+            int sn = Convert.ToInt32(txtSecondNumber.Text);
+
+            fn += 5;
+            sn -= 10;
+
+            string msg = "First Number is increased by 5. RESULT = " + fn.ToString();
+            MessageBox.Show(msg);
+
+            msg = "Second number is decreased by 10. RESULT = " + sn.ToString();
+            MessageBox.Show(msg);
+        }
+    }
+}
